4adf00c44ae9ca9ab488f4b8f5939acde5a22f02
somnathout <somnath@outspokn.co>
2026-05-18 16:39:36 +0530

    feat: integrate Google Tag Manager and add Google site verification to root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,13 @@
 import type { Metadata } from "next";
 import { Manrope, Sora, Space_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
+import LocationHeader from "@/components/layout/LocationHeader";
+import LocationInitializer from "@/components/common/LocationInitializer";
+import BottomNav from "@/components/layout/BottomNav";
+import ReduxProvider from "@/components/providers/ReduxProvider";
+import { ThemeProvider } from "@/components/providers/ThemeProvider";
+import { Toaster } from "@/components/ui/sonner";
 
 const displayFont = Sora({
   variable: "--font-display",
@@ -28,14 +35,10 @@ export const metadata: Metadata = {
     shortcut: '/favIcon.svg',
     apple: '/favIcon.svg',
   },
+  verification: {
+    google: "sv5T6Y3mi1OfLRxLnZlu5OTnOtP_2KC1NtA0sRiMm8A",
+  },
 };
-
-import LocationHeader from "@/components/layout/LocationHeader";
-import LocationInitializer from "@/components/common/LocationInitializer";
-import BottomNav from "@/components/layout/BottomNav";
-import ReduxProvider from "@/components/providers/ReduxProvider";
-import { ThemeProvider } from "@/components/providers/ThemeProvider";
-import { Toaster } from "@/components/ui/sonner";
 
 export default function RootLayout({
   children,
@@ -44,9 +47,24 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Script id="gtm-script" strategy="afterInteractive">
+        {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5KTLS2MS');`}
+      </Script>
       <body
         className={`${displayFont.variable} ${bodyFont.variable} ${monoFont.variable} antialiased bg-background text-foreground pb-20 md:pb-0`}
       >
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5KTLS2MS"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <ReduxProvider>
           <ThemeProvider>
             <LocationInitializer />
@@ -66,4 +84,3 @@ export default function RootLayout({
     </html>
   );
 }
-
